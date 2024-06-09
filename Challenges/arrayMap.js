7b08adbeabc9db83800cabdd5e9fb0dce183cad1
landry kassius <lkassindi@gmail.com>
2024-06-09 09:30:42 +0200

    add challenges

---
+++ Challenges/arrayMap.js
@@ -0,0 +1,21 @@
+function subarrayWithSum(arr, target) {
+    let currentSum = 0;
+    let start = 0;
+
+    for (let end = 0; end < arr.length; end++) {
+        currentSum += arr[end];
+
+        while (currentSum > target && start <= end) {
+            currentSum -= arr[start];
+            start++;
+        }
+
+        if (currentSum === target) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(subarrayWithSum(arr, target)); 
